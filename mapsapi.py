865dc1c9c4a974658580b7c9cc9e837c5c6a1ea9
nickyfoster <nik9516@gmail.com>
2019-09-28 18:51:08 +0300

    morpho added

--- mapsapi.py
+++ mapsapi.py
@@ -1,41 +1,47 @@
 import googlemaps
+
 import auth
 
-gmaps = googlemaps.Client(key=google_api_key)
+gmaps = googlemaps.Client(key=auth.google_maps_API)
+
 SPB = (59.935552, 30.321777)
 
 
-class PyMapsAPI():
+class PyMapsAPI:
     """Custom Google Maps API class for searching new place."""
+    """Nick: remember to set_coordinates first!"""
 
-    def __init__(self):
-        self.gmaps = googlemaps.Client(key=auth.google_maps_API)
-        self.coordinates = None
+    def __init__(self, language='ru', radius=5000):
+        self.gmaps = googlemaps.Client(key=auth.google_maps_API)  # Google Maps API initialization
+        self.coordinates = (59.935552, 30.321777)  # The lat/lon center point values. Default: Saint-Petersburg center
+        self.language = language  # The language in which to return results.
+        self.radius = radius  # Distance in meters within which to bias results.
 
-    def set_custom_coordinates(self, custom_coordinates):
+    def set_coordinates(self, custom_coordinates):
         self.coordinates = custom_coordinates
 
     def get_places_by_query(self, search_text):
-        return self.gmaps.places_autocomplete_query(input_text=search_text, location=SPB, radius=5000, language='ru')
+        return self.gmaps.places_autocomplete_query(input_text=search_text, location=self.coordinates,
+                                                    radius=self.radius, language=self.language)
+
+    def get_place_info_by_id(self, place_id):
+        place_info = self.gmaps.place(place_id=place_id, language='ru')
+        return place_info
 
     def get_formatted_places(self, search_text):
+        formatted_places = []
+        unformatted_places = self.gmaps.places_autocomplete_query(input_text=search_text, location=self.coordinates,
+                                                                  radius=self.radius, language=self.language)
+        for place in unformatted_places:
+            try:
+                formatted_places.append({'place_description': place['description'], 'place_id': place['place_id']})
+            except Exception as e:
+                print(f"Error: {e}")
+        return formatted_places
 
+    def get_recommended_places(self, true_user_keywords):
+        recommended_places = []
+        for keyword in true_user_keywords:
+            recommended_places.append(self.get_formatted_places(keyword))
+        return recommended_places
 
-
-
-def get_places_info(search_text):
-    user_places = gmaps.places_autocomplete_query(input_text=search_text, location=SPB, radius=5000, language='ru')
-    return user_places
-
-
-def find_places(search_text):
-    places_to_return = []
-    user_places = gmaps.places_autocomplete_query(input_text=search_text, location=SPB, radius=5000, language='ru')
-    for place in user_places:
-        places_to_return.append({'place_description': place['description'], 'place_id': place['place_id']})
-    return places_to_return
-
-
-def get_place_info(place_id):
-    place_info = gmaps.place(place_id=place_id, language='ru')
-    return place_info
--- morpho.py
+++ morpho.py
@@ -1,43 +1,46 @@
-from rutermextract import TermExtractor
+import rutermextract
 
-term_extractor = TermExtractor()
+import keywords
 
 
-def group_morpho_analysis(groups: dict, limit: int, frequency: int) -> str:
-    preprocess_text_data = ""
-    for group in groups:
-        try:
-            preprocess_text_data += group['description'] + group['name']
-        except Exception as e:
-            print(f"Error: {e}")
+class PyMorpho:
+    def __init__(self):
+        self.termex = rutermextract.TermExtractor()
+        self.true_keywords = keywords.our_keywords
 
-    analysed_data = {}
+    def get_keywords_list(self, raw_text_data, limit, frequency):
+        """ Generate keywords list from string
+        term.normalized - keyword in text
+        term.count      - quantity of keyword in text
+        """
+        keyword_count_dict = {}
+        result = []
 
-    """
-    term.normalized - keyword in text
-    term.count      - quantity of keyword in text
-    """
-    for term in term_extractor(preprocess_text_data, limit=limit):
-        analysed_data[term.normalized] = term.count
+        for term in self.termex(raw_text_data, limit=limit):
+            keyword_count_dict[term.normalized] = term.count
 
-    result = []
-    for keyword, count in analysed_data.items():
-        if count > frequency:
-            result.append(keyword)
+        for keyword, count in keyword_count_dict.items():
+            if count > frequency:
+                result.append(keyword)
+        return result
 
-    return result
+    def get_user_groups_keywords(self, groups, limit=40, frequency=2):
 
-def generate_dict(raw_text_data, limit, frequency):
-    analysed_data = {}
-    """
-    term.normalized - keyword in text
-    term.count      - quantity of keyword in text
-    """
-    for term in term_extractor(raw_text_data, limit=limit):
-        analysed_data[term.normalized] = term.count
-    result = []
-    for keyword, count in analysed_data.items():
-        if count > frequency:
-            result.append(keyword)
-    return result
+        """ Generate keywords list according to user groups """
+        preprocess_text_data = ""
+        keyword_count_dict = {}
+        user_groups_keywords = []
+        for group in groups:
+            try:
+                preprocess_text_data += group['description'] + group['name']
+            except Exception as e:
+                print(f"Error: {e}")
 
+        for term in self.termex(preprocess_text_data, limit=limit):
+            keyword_count_dict[term.normalized] = term.count
+
+        for keyword, count in keyword_count_dict.items():
+            if count > frequency:
+                user_groups_keywords.append(keyword)
+
+        return user_groups_keywords
