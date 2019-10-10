f87499cec13679800e40db3fe8a9166d3dcf8107
nickyfoster <nik9516@gmail.com>
2019-10-10 22:22:31 +0300

    project major update

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 import os
 
-from app import app, db
+from flask_api_server.app import app, db
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
---
+++ resources/env.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+TEST_HOME = Path(__file__).parents[0]
+RESOURCES = TEST_HOME / 'resources'
+USER_FRIENDS_DATA = RESOURCES / 'user_friends_data.json'
+USERS_GROUPS_DATA = RESOURCES / 'users_groups_data.json'
+PLACES_DATA = RESOURCES / 'places_data.json'
